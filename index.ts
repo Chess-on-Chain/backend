a6a0c3a5cb866efdf71fe9404f1e1885760491b3
salismazaya <salismazaya@gmail.com>
2025-07-22 22:10:46 +0700

    ✨ (env,cors): add CORS_ORIGIN env variable and enable cors middleware ⬆️ (deps): add cors package and its types as dependencies 🔧 (index.ts): configure cors middleware to use CORS_ORIGIN from env

--- index.ts
+++ index.ts
@@ -2,9 +2,16 @@ import express from "express";
 import { sequelize } from "./models/database";
 import usersRouter from "./routes/users";
 import roomsRouter from "./routes/rooms";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN as string,
+  })
+);
 
 app.use("/users", usersRouter);
 app.use("/rooms", roomsRouter);
