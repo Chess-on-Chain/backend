79c2ff104cfa45126ae5a74a55085e851b6eecfa
salismazaya <salismazaya@gmail.com>
2025-07-24 11:15:07 +0700

    ✨ (index.ts): allow multiple CORS origins by splitting CORS_ORIGIN env variable 🔧 (index.ts): fix missing semicolons for consistent code style

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { sequelize } from "./models/database";
 import usersRouter from "./routes/users";
 import roomsRouter from "./routes/rooms";
-import webhookRouter from "./routes/webhook"
+import webhookRouter from "./routes/webhook";
 import cors from "cors";
 
 const app = express();
@@ -10,13 +10,13 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN as string,
+    origin: (process.env.CORS_ORIGIN as string).split(","),
   })
 );
 
 app.use("/users", usersRouter);
 app.use("/rooms", roomsRouter);
-app.use("/webhook", webhookRouter)
+app.use("/webhook", webhookRouter);
 
 sequelize.sync().then(() => {
   const port = process.env.PORT || 3000;
